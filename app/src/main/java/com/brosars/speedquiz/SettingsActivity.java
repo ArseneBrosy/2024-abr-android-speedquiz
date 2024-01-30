ff87e02c62b31f42995fae894581a7fbdc27d515
ArseneBrosy <arsene.brosy@divtec.ch>
2024-01-30 12:27:09 +0100

    get number of questions

--- app/src/main/java/com/brosars/speedquiz/SettingsActivity.java
+++ app/src/main/java/com/brosars/speedquiz/SettingsActivity.java
@@ -1,6 +1,8 @@
 package com.brosars.speedquiz;
 
 import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.DragEvent;
 import android.view.View;
@@ -8,13 +10,17 @@ import android.view.View;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.brosars.speedquiz.controllers.Question;
+import com.brosars.speedquiz.models.SpeedQuizSQLite;
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.slider.Slider;
 
-public class SettingsActivity extends AppCompatActivity {
+import java.util.ArrayList;
 
+public class SettingsActivity extends AppCompatActivity {
     private MaterialButton okButton;
     private Slider questionSpeedSlider;
+    private int maxQuestionCount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,11 +35,21 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onStart() {
         super.onStart();
 
+        // get settings for questions speed
         SharedPreferences sharedPref = getSharedPreferences("IDValue", 0);
         int questionSpeed = sharedPref.getInt("settings_questions_speed", 5000);
         questionSpeedSlider.setValue(questionSpeed / 1000F);
         System.out.println("preferences");
         System.out.println(questionSpeed);
+
+        // get max question count
+        SpeedQuizSQLite helper = new SpeedQuizSQLite(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT COUNT(idQuiz) FROM quiz", null);
+        cursor.moveToFirst();
+        maxQuestionCount = cursor.getInt(0);
+        cursor.close();
+        db.close();
 
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
