ac13e88a458c2201ff59412ee811b8251932c86b
ArseneBrosy <arsene.brosy@divtec.ch>
2024-02-06 10:42:51 +0100

    questions end

--- app/src/main/java/com/brosars/speedquiz/QuestionsActivity.java
+++ app/src/main/java/com/brosars/speedquiz/QuestionsActivity.java
@@ -1,15 +1,25 @@
 package com.brosars.speedquiz;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.brosars.speedquiz.models.SpeedQuizSQLite;
 import com.google.android.material.button.MaterialButton;
 
 public class QuestionsActivity extends AppCompatActivity {
 
     private MaterialButton cancelButton;
+    private MaterialButton addButton;
+    private EditText labelEditText;
+    private CheckBox isTrueCheckbox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +27,9 @@ public class QuestionsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_questions);
 
         cancelButton = findViewById(R.id.cancel_button);
+        addButton = findViewById(R.id.add_button);
+        labelEditText = findViewById(R.id.question_label);
+        isTrueCheckbox = findViewById(R.id.checkbox_is_true);
     }
 
     @Override
@@ -29,5 +42,39 @@ public class QuestionsActivity extends AppCompatActivity {
                 finish();
             }
         });
+
+        labelEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                String text = labelEditText.getText().toString();
+                addButton.setEnabled(text.length() > 0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addQuestion(labelEditText.getText().toString(), isTrueCheckbox.isChecked());
+                finish();
+            }
+        });
+    }
+
+    private void addQuestion(String label, boolean isTrue) {
+        String query = "INSERT INTO quiz VALUES (null, \"" + label + "\", " + (isTrue ? "1" : "0") + ");";
+        SpeedQuizSQLite helper = new SpeedQuizSQLite(this);
+        SQLiteDatabase db = helper.getReadableDatabase();
+        db.execSQL(query);
+        db.close();
     }
 }
